d92fe1aee40b7907f3c45f467761ddd3dca4ea32
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2019-05-24 07:06:14 -0300

    improve bootstrap

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 - run a Kafka cluster https://customer.cloudkarafka.com
 - go to `details` menu, download the `connection details` to this project and
   rename it to `.env`
-- run `docker build -t ruby-kafka`
-- run `docker run ruby-kafka ruby main.rb`
+- run `docker build -t ruby-kafka . && docker run -it ruby-kafka ruby main.rb`
--- main.rb
+++ main.rb
@@ -1,4 +1,50 @@
 require 'dotenv/load'
+require './kafka'
 
-require './consumer'
-require './producer'
+def start_producer
+  puts 'start producer'
+  kafka = Rdkafka::Config.new(CONFIG)
+  producer = kafka.producer
+
+  10.times do |i|
+    puts "Producing message #{i}"
+    producer.produce(
+      topic: default_topic,
+      payload: i.to_s,
+      key: i.to_s
+    ).wait
+  end
+end
+
+def start_consumer(group:, instance:)
+  puts "start consumer #{group}#{instance}"
+  kafka = Rdkafka::Config.new(CONFIG.merge(:'group.id' => group))
+  consumer = kafka.consumer
+
+  consumer.subscribe(default_topic)
+
+  begin
+    consumer.each do |message|
+      puts "Consumer #{group}#{instance} received: #{summary(message)}"
+    end
+  rescue Rdkafka::RdkafkaError => e
+    retry if e.is_partition_eof?
+    raise e
+  end
+end
+
+def summary(message)
+  "payload = #{message.payload}, partition = #{message.partition}, offset = #{message.offset}"
+end
+
+def default_topic
+  "#{ENV['CLOUDKARAFKA_TOPIC_PREFIX']}default"
+end
+
+[
+  Thread.new { start_producer },
+  Thread.new { start_consumer(group: 'B', instance: 1) },
+  Thread.new { start_consumer(group: 'A', instance: 1) },
+  Thread.new { start_consumer(group: 'A', instance: 2) }
+].each(&:join)
+
